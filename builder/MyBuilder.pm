cb22ca506c6ec3fe6631270165b65dd6aeea1cfe
Ben Bullock <benkasminbullock@gmail.com>
2016-12-12 22:28:52 +0900

    Make it work on BSD by substituting gmake

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -36,10 +36,14 @@ sub is_debug {
 
 sub ACTION_build {
     my $self = shift;
+    my $make = 'make';
+    if ($^O =~ /(free|net)bsd/i) {
+	$make = 'gmake';
+    }
     $self->ACTION_ppport_h() unless -e 'ppport.h';
     unless (-f "$LIBZSTD_DIR/libzstd.a") {
         local $ENV{CFLAGS} = '-O3 -fPIC';
-        $self->do_system('make' => '-C', $LIBZSTD_DIR, 'libzstd');
+        $self->do_system($make => '-C', $LIBZSTD_DIR, 'libzstd');
     }
     $self->do_system('rm', '-f', glob("$LIBZSTD_DIR/*.$Config{so}"));
     $self->SUPER::ACTION_build();
